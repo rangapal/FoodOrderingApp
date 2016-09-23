97bcb51f5bdff6deeba2d996b268c1d3e31565a2
Khychhim <limkhychhim@yahoo.com>
2016-09-23 22:28:33 +1200

    Add Menu Display Add Login

---
+++ app/src/androidTest/java/com/foodorderingapp/Test.java
@@ -0,0 +1,13 @@
+package com.foodorderingapp;
+
+/**
+ * Created by User on 9/23/2016.
+ */
+public class Test {
+
+    //@Test
+    public void testScrollingAt()throws Exception{
+
+        //onData(hasEntry(equalTo(ListViewSample.)))
+    }
+}
